9ae3e717763757ed9905a9b07ab1fc9ba4a32758
Alexander Snitko <alexander.snitko@specific-group.com>
2021-12-21 17:25:20 +0300

    changed tag prototype

--- src/atoms/tag-creator/TagCreator.ts
+++ src/atoms/tag-creator/TagCreator.ts
@@ -1,7 +1,7 @@
 export default class TagCreator {
   constructor(tagName: string) {
     const tag: any = document.createElement(tagName, { is: tagName });
-    tag.__proto__ = TagCreator.prototype;
+    tag.__proto__ = this;
 
     return tag;
   }
