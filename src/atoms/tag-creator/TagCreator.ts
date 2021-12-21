d8e89b10b238545bee61461ef72add49bdb02fcb
Alexander Snitko <alexander.snitko@specific-group.com>
2021-12-21 16:41:04 +0300

    removed extra

--- src/atoms/tag-creator/TagCreator.ts
+++ src/atoms/tag-creator/TagCreator.ts
@@ -1,14 +1,6 @@
 export default class TagCreator {
-  static tagOldPrototype: any;
-
   constructor(tagName: string) {
-    return TagCreator.#createTag(tagName);
-  }
-
-  static #createTag(tagName: string): TagCreator {
     const tag: any = document.createElement(tagName, { is: tagName });
-    this.tagOldPrototype = tag.__proto__;
-
     tag.__proto__ = TagCreator.prototype;
 
     return tag;
